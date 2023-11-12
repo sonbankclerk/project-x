78e747e1143027c557c4a0aeafdccd0e72cd16c4
sony <thsgoddnjs@gmail.com>
2023-11-12 23:54:34 +0900

    1 week 7 update

--- src/projectX/HelloBiodome07.java
+++ src/projectX/HelloBiodome07.java
@@ -1,5 +1,7 @@
 package projectX;
 
+import java.util.Arrays;
+
 public class HelloBiodome07 {
     static String message(String a){
         String b = a.toUpperCase();
@@ -9,31 +11,49 @@ public class HelloBiodome07 {
     }
 
     public static void main(String[] args){
-//        args = new String[]{" jjjcccg EEEYYYeee"};
+        args = new String[]{" jjjccc EEEEYYYeee"};
         String a = args[0];
 
 //        final String[] NUCLEOTIDE = new String[]{"C","Y","J","E","H"};
         final char[] NUCLEOTIDE = new char[]{'C','Y','J','E','H'};
 
         char[] list = message(a).toCharArray();
-//        System.out.println(Arrays.toString(list));
+        System.out.println(Arrays.toString(list));
 
-        // 문자열을 저장할 변수
-        String output ="";
 
-        int count = 1;
-        for (int i = 1; i < list.length; i++) {
-            if (list[i] == list[i - 1]) {
-                count++;
-            } else {
-                output += list[i-1]+count;
-                count = 1;
+        String result = "";
+        int count =0;
+
+        //맞는 염기서열
+        for (int i=0; i<list.length; i++){
+            for(int j=0; j< NUCLEOTIDE.length; j++){
+                if(NUCLEOTIDE[j] == list[i]){
+                    result += NUCLEOTIDE[j];
+                }
             }
         }
 
-        output += list[list.length - 1] + count;
-        System.out.println(list);
-        System.out.print("->"+output.toString());
+        for(int i=0; i<result.length(); i++){
+
+        }
+
+
+        // 문자열을 저장할 변수
+//        String output ="";
+//
+//        int count = 0;
+//        for (int i = 0; i < list.length; i++) {
+//            if (list[i] == list[i - 1]) {
+//                count++;
+//            } else {
+//                output += list[i-1]+count;
+//                count = 1;
+//            }
+//        }
+
+//        output += list[list.length - 1] + count;
+//        System.out.println(list);
+//        System.out.print("->"+output);
 
     }
 }
